bb4cdfa5d510f5377926ac00a94df09b4851bd90
Anatoly Semenov <anatolysrmenov@gmail.com>
2022-09-19 15:43:19 +0300

    Global use docs components

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,18 @@ import router from "./router"
 
 // Ui
 import AntDesign from "ant-design-vue"
+import componentCode from "@/components/component-code.vue"
+import propsColumns from "@/components/props-columns.vue"
 
 // Css
 import "ant-design-vue/dist/antd.css"
+import "vue-code-highlight/themes/duotone-sea.css"
+import "vue-code-highlight/themes/window.css"
 import "./assets/styles/styles.styl"
 
-createApp(App).use(router).use(AntDesign).mount("#app")
+createApp(App)
+	.use(router)
+	.use(AntDesign)
+	.component("component-code", componentCode)
+	.component("props-columns", propsColumns)
+	.mount("#app")
