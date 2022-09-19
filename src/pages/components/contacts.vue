171dfc1192c2a394929e7983ef77de6f4f8a2710
Anatoly Semenov <anatolysrmenov@gmail.com>
2022-09-19 17:10:11 +0300

    Create ui-contacts component

---
+++ src/pages/components/contacts.vue
@@ -0,0 +1,68 @@
+<template>
+	<div class="doc">
+		<p class="doc__title">Ui-contacts</p>
+		<div class="doc__component">
+			<ui-contacts :data="dataProp" />
+		</div>
+
+		<component-code
+			name="ui-contacts"
+			:componentProperties="{
+				data: dataProp
+			}"
+		/>
+
+		<a-table :columns="columns" :data-source="data" :pagination="false">
+			<template #bodyCell="{ column, record }">
+				<props-columns :column="column" :record="record" />
+			</template>
+		</a-table>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+
+// Components
+import uiContacts from "@/components/ui-kit/ui-contacts.vue"
+
+// Hooks
+import useDocs from "@/hooks/docs"
+const { columns } = useDocs()
+
+// Types
+import type { Docs, UiContacts } from "@/types"
+
+// Props
+const dataProp = ref<UiContacts.Data>([
+	{
+		contact: "Anatoly Semenov",
+		name: "Username"
+	},
+	{
+		contact: "+79514703322",
+		name: "Mobile phone"
+	},
+	{
+		contact: "+79514703321",
+		name: "Direct phone"
+	},
+	{
+		contact: "email@gmail.com",
+		name: "E-mail"
+	},
+	{
+		contact: "google.com",
+		name: "Website"
+	}
+])
+
+const data: Docs.Data = [
+	{
+		key: "1",
+		property: "data",
+		type: "Array",
+		default: "[]"
+	}
+]
+</script>
