238f06999af1fa8898ce4e69b00479b6ffd742b7
Anatoly Semenov <anatolysrmenov@gmail.com>
2022-09-19 17:08:19 +0300

    Create ui-tooltip component

---
+++ src/components/ui-kit/ui-tooltip.vue
@@ -0,0 +1,46 @@
+<template>
+	<a-tooltip
+		class="ui-tooltip"
+		:overlayClassName="overlayClassName"
+		:placement="placement"
+		:title="title"
+		:trigger="trigger"
+	>
+		<template #title>
+			<slot name="title" />
+		</template>
+		<slot />
+	</a-tooltip>
+</template>
+
+<script setup lang="ts">
+import { defineProps, PropType } from "vue"
+
+// Components
+import ATooltip from "ant-design-vue/lib/tooltip"
+
+// Types
+import { UiTooltip } from "@/types"
+
+const props = defineProps({
+	placement: {
+		type: String as PropType<UiTooltip.Placement>,
+		default: "top"
+	},
+	title: String,
+	trigger: {
+		type: String as PropType<UiTooltip.Trigger>,
+		default: "hover"
+	},
+	overlayClassName: {
+		type: String,
+		default: "ui-tooltip__wrapper"
+	}
+})
+</script>
+
+<style lang="stylus" scoped>
+.ui-tooltip
+  &__wrapper
+    opacity 1
+</style>
