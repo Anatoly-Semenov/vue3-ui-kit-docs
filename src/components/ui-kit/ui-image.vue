0f725a097166346cbf59f6b4f04679a9315b5ceb
Anatoly Semenov <anatolysrmenov@gmail.com>
2022-09-19 17:12:33 +0300

    Create ui-image component

---
+++ src/components/ui-kit/ui-image.vue
@@ -0,0 +1,45 @@
+<template>
+	<a-image
+		class="ui-image"
+		:alt="alt"
+		:fallback="fallback"
+		:placeholder="placeholder"
+		:preview="preview"
+		:src="src"
+		:height="height"
+		:width="width"
+	>
+		<slot name="previewMask" />
+		<slot name="placeholder" />
+	</a-image>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue"
+
+// Components
+import AImage from "ant-design-vue/lib/image"
+
+const props = defineProps({
+	// Image description
+	alt: String,
+	// Load failure fault-tolerant src
+	fallback: String,
+	// Image height
+	height: [String, Number],
+	// Image width
+	width: [String, Number],
+	// Load placeholder, use default placeholder when set true
+	placeholder: Boolean,
+	preview: {
+		type: Boolean,
+		default: false
+	},
+	src: String
+})
+</script>
+
+<style lang="stylus" scoped>
+.ui-image
+  opacity 1
+</style>
