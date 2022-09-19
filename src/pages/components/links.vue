32717057b6e65cb0d9ed052291af9266270141b2
Anatoly Semenov <anatolysrmenov@gmail.com>
2022-09-19 17:10:55 +0300

    Create ui-links component

---
+++ src/pages/components/links.vue
@@ -0,0 +1,56 @@
+<template>
+	<div class="doc">
+		<p class="doc__title">Ui-links</p>
+		<div class="doc__component" style="width: 300px">
+			<ui-links :data="linksData" />
+		</div>
+
+		<component-code
+			name="ui-links"
+			:componentProperties="{
+				data: linksData
+			}"
+		/>
+
+		<a-table :columns="columns" :data-source="data" :pagination="false" />
+	</div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from "vue"
+
+// Components
+import uiLinks from "@/components/ui-kit/ui-links.vue"
+
+// Types
+import { Docs, UiLinks } from "@/types"
+
+// Hooks
+import useDocs from "@/hooks/docs"
+const { columns } = useDocs()
+
+const data: Docs.Data = [
+	{
+		key: "1",
+		property: "data",
+		type: "Array",
+		default: "[]"
+	}
+]
+
+// Props
+const linksData: UiLinks.Data = [
+	{
+		text: "ui-button",
+		link: "/base/ui-button"
+	},
+	{
+		text: "ui-avatar",
+		link: "/base/ui-avatar"
+	},
+	{
+		text: "ui-badge",
+		link: "/base/ui-badge"
+	}
+]
+</script>
---
+++ src/types/ui-links.ts
@@ -0,0 +1,8 @@
+export namespace UiLinks {
+	export type Data = Item[]
+
+	export interface Item {
+		text: string
+		link: string
+	}
+}
